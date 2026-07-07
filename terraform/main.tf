6651ec7a6e3349b9d5d99f06b0d49bf2117f0caa
Emmanuel Audain <ezbasic954@gmail.com>
2026-07-07 23:12:57 +0000

    Phase 3: Linux VM workload

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,3 +79,62 @@ resource "azurerm_subnet_network_security_group_association" "app" {
   subnet_id                 = azurerm_subnet.app.id
   network_security_group_id = azurerm_network_security_group.app.id
 }
+
+# ---------------------------------------------------------------------------
+# Phase 3 - Workload Target (Linux VM)
+# ---------------------------------------------------------------------------
+
+resource "azurerm_public_ip" "vm" {
+  name                = "pip-vm-${local.name_prefix}-001"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = local.common_tags
+}
+
+resource "azurerm_network_interface" "vm" {
+  name                = "nic-vm-${local.name_prefix}-001"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.common_tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.app.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "app" {
+  name                            = "vm-${local.name_prefix}-001"
+  location                        = azurerm_resource_group.main.location
+  resource_group_name             = azurerm_resource_group.main.name
+  size                            = var.vm_size
+  admin_username                  = var.admin_username
+  network_interface_ids           = [azurerm_network_interface.vm.id]
+  disable_password_authentication = true
+  tags                            = local.common_tags
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
